b4f17c543cb68dbf8ac1069fbb4168c0c41eb3a1
Azharrabbani <azharr1203@gmail.com>
2024-12-16 23:31:39 +0700

    Finished the apps

--- creovate/settings.py
+++ creovate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+sneou5hkjc^%nx7wssgu&9yd-*9_vd*3g9g!qa!%!ews+ts6v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
